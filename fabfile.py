0dfd11ed6ab4a84729672a2f05e574d647084a83
Mr Snow <mr.snow@houseoflaudanum.com>
2012-03-08 00:28:22 +1100

    PEP8.

--- fabfile.py
+++ fabfile.py
@@ -5,89 +5,92 @@ import glob
 
 
 def capture(interval=5, source_directory=None):
-  """
-  Capture stills to the local drive.
-  """
-  interval = int(interval)
-  if not source_directory:
-    source_directory = datetime.now().strftime("%Y%m%d-%H%M%S")
-    print source_directory
-    try:
-      os.mkdir(source_directory)
-    except:
-      pass
-  with cd(source_directory):
+    """
+    Capture stills to the local drive.
+    """
+    interval = int(interval)
+    if not source_directory:
+        source_directory = datetime.now().strftime("%Y%m%d-%H%M%S")
+        print source_directory
+        try:
+            os.mkdir(source_directory)
+        except:
+            pass
+    with cd(source_directory):
 # http://sourceforge.net/apps/mediawiki/gphoto/index.php?title=Capture_examples
 #Capture_Targets
 
-    try:
-      local("gphoto2 \
-        --set-config capturetarget=0 \
-        --capture-image-and-download \
-        -I %d"
-        % (interval))
-    except:
-      pass
+        try:
+            local("gphoto2 \
+                --set-config capturetarget=0 \
+                --capture-image-and-download \
+                -I %d"
+                % (interval))
+        except:
+            pass
 
-  return source_directory
+    return source_directory
 
 
 def composite(source_directory=".", extension="jpg", dest_file="output.mp4",
-  rotate=None, size=(1920, 1024)):
-  """
-  Stitch stills in to a compliant mp4 video.
-  @todo Non-square pixels at HD?
-  """
+    rotate=None, size=(1920, 1024)):
+    """
+    Stitch stills in to a compliant mp4 video.
+    @todo Non-square pixels at HD?
+    """
 # if the input format isn't understood by ffmpeg normalize the images first
-  new_extension = extension
-  preprocess = False
-  args = [
-    "-size %dx%d" % (size[0], size[0]),
-    "-resize %dx%d" % (size[0], size[0]),
-    "-gravity Center",
-    "-crop %dx%d+0+0 +repage" % (size[0], size[1]),
-    "-depth 8",
-    "-colorspace RGB"]
-  if extension == "cr2":
-    args.append(" -format png")
-    new_extension = "png"
-  if rotate:
-    args.append("-rotate %d" % int(rotate))
-  if preprocess:
-    local("mogrify %s %s/*.%s" % (" ".join(args), source_directory, extension))
+    new_extension = extension
+    preprocess = False
+    args = [
+        "-size %dx%d" % (size[0], size[0]),
+        "-resize %dx%d" % (size[0], size[0]),
+        "-gravity Center",
+        "-crop %dx%d+0+0 +repage" % (size[0], size[1]),
+        "-depth 8",
+        "-colorspace RGB"]
+    if extension == "cr2":
+        args.append(" -format png")
+        new_extension = "png"
+    if rotate:
+        args.append("-rotate %d" % int(rotate))
+    if preprocess:
+        local("mogrify %s %s/*.%s"
+            % (" ".join(args), source_directory, extension))
 
-#  command = "ffmpeg -f image2 -r 25 -i %s/%s.%s -b 1000k %s" %
+#    command = "ffmpeg -f image2 -r 25 -i %s/%s.%s -b 1000k %s" %
 # (source_directory, "%d", new_extension, dest_file)
 
 #	loop through the directory symlinking images to their index numbers
-  i = 1
-  try:
-    local("rm %s/symlink-*.%s" % (source_directory, new_extension))
-  except:
-    pass
-  for filename in sorted(glob.glob(os.path.join(source_directory, '*.%s' % new_extension))):
-    symlink = os.path.join(source_directory, 'symlink-%d.%s' % (i, new_extension))
-    local("ln -sf %s %s" % (filename, symlink))
-    i += 1
+    i = 1
+    try:
+        local("rm %s/symlink-*.%s" % (source_directory, new_extension))
+    except:
+        pass
+    for filename in sorted(glob.glob(os.path.join(source_directory, '*.%s'
+        % new_extension))):
+        symlink = os.path.join(source_directory, 'symlink-%d.%s'
+            % (i, new_extension))
+        local("ln -sf %s %s" % (filename, symlink))
+        i += 1
 # sudo find /usr/share/ffmpeg -iname '*.ffpreset'
 
-  command = "ffmpeg -f image2 \
-    -r 25 \
-    -i %s/%s.%s \
-    -ab 1000k \
-    -vb 3000k \
-    -vpre libx264-hq \
-    -acodec libfaac \
-    -vcodec libx264 \
-    %s" % (source_directory, "symlink-%d", new_extension, dest_file)
-  error = local(command, capture=True)
-  print error
-  
-  local("rm %s/symlink-*.%s" % (source_directory, new_extension))
+    command = "ffmpeg -f image2 \
+        -r 25 \
+        -i %s/%s.%s \
+        -ab 1000k \
+        -vb 3000k \
+        -vpre libx264-hq \
+        -acodec libfaac \
+        -vcodec libx264 \
+        %s" % (source_directory, "symlink-%d", new_extension, dest_file)
+    error = local(command, capture=True)
+    print error
+
+    local("rm %s/symlink-*.%s" % (source_directory, new_extension))
 
 
 def distribute():
-  """
-  Upload to a social video service.
-  """
-  pass
+    """
+    Upload to a social video service.
+    """
+    pass
